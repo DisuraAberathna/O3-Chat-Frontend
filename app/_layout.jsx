83920dd50fb7d3fa0aaf65d7263e668d70b19079
Disura Aberathna <disura2005@gmail.com>
2025-11-03 11:48:27 +0530

    update navigation bar color

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,46 +1,49 @@
 import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
+    DarkTheme,
+    DefaultTheme,
+    ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import {useFonts} from "expo-font";
+import {Stack} from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { StatusBar } from "expo-status-bar";
+import {useEffect} from "react";
+import {useColorScheme} from "@/hooks/useColorScheme";
+import {StatusBar} from "expo-status-bar";
 import "react-native-reanimated";
+import * as SystemUI from 'expo-system-ui';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    "snap-itc": require("@/assets/fonts/snap-itc.ttf"),
-  });
+    const colorScheme = useColorScheme();
+    const [loaded] = useFonts({
+        "snap-itc": require("@/assets/fonts/snap-itc.ttf"),
+    });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    colorScheme === "dark" && SystemUI.setBackgroundColorAsync('transparent');
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(other)/help" options={{ headerShown: false }} />
-        <Stack.Screen name="(other)/new-chat" options={{ headerShown: false }} />
-        <Stack.Screen name="(other)/chat" options={{ headerShown: false }} />
-        <Stack.Screen name="(other)/verify" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar backgroundColor="#0C4EAC" style="light" />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+            <Stack>
+                <Stack.Screen name="index" options={{headerShown: false}}/>
+                <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                <Stack.Screen name="(other)/help" options={{headerShown: false}}/>
+                <Stack.Screen name="(other)/new-chat" options={{headerShown: false}}/>
+                <Stack.Screen name="(other)/chat" options={{headerShown: false}}/>
+                <Stack.Screen name="(other)/verify" options={{headerShown: false}}/>
+            </Stack>
+            <StatusBar backgroundColor="#0C4EAC" style="light"/>
+        </ThemeProvider>
+    );
 }
