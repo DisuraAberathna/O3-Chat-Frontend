f9037df33f3b5860eab3b1c2af9d914c6b0a0dae
Disura Aberathna <disura2005@gmail.com>
2026-02-04 21:28:54 +0530

    Update _layout.jsx

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -18,6 +18,11 @@ const AuthLayout = () => {
         <Stack.Screen name="register-form-1" options={{ headerShown: false }} />
         <Stack.Screen name="register-form-2" options={{ headerShown: false }} />
       </Stack>
+      <StatusBar
+        style={colorScheme === "dark" ? "light" : "dark"}
+        backgroundColor={colorScheme === "dark" ? "#111827" : "#e2e8f0"}
+        translucent={false}
+      />
     </ThemeProvider>
   );
 };
