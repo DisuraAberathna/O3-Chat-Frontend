5037c709fe88c4ad69ed9f7df562f40e08333a20
Disura Aberathna <disura2005@gmail.com>
2026-02-04 21:29:22 +0530

    Update _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,14 +3,16 @@ import {
     DefaultTheme,
     ThemeProvider,
 } from "@react-navigation/native";
-import {useFonts} from "expo-font";
-import {Stack} from "expo-router";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import {useEffect} from "react";
-import {useColorScheme} from "@/hooks/useColorScheme";
-import {StatusBar} from "expo-status-bar";
+import { useEffect } from "react";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import * as SystemUI from 'expo-system-ui';
+
+import { AlertProvider } from "@/components/AlertProvider";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -34,16 +36,19 @@ export default function RootLayout() {
 
     return (
         <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-            <Stack>
-                <Stack.Screen name="index" options={{headerShown: false}}/>
-                <Stack.Screen name="(auth)" options={{headerShown: false}}/>
-                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-                <Stack.Screen name="(other)/help" options={{headerShown: false}}/>
-                <Stack.Screen name="(other)/new-chat" options={{headerShown: false}}/>
-                <Stack.Screen name="(other)/chat" options={{headerShown: false}}/>
-                <Stack.Screen name="(other)/verify" options={{headerShown: false}}/>
-            </Stack>
-            <StatusBar backgroundColor="#0C4EAC" style="light"/>
+            <AlertProvider>
+                <Stack>
+                    <Stack.Screen name="index" options={{ headerShown: false }} />
+                    <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                    <Stack.Screen name="(other)" options={{ headerShown: false }} />
+                </Stack>
+                <StatusBar
+                    style={colorScheme === "dark" ? "light" : "dark"}
+                    backgroundColor="transparent"
+                    translucent={true}
+                />
+            </AlertProvider>
         </ThemeProvider>
     );
 }
